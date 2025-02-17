db168465ee85fd3e1e0e3957e49d91ed30fcac05
Samuel Shumake <shumakesam@gmail.com>
2025-02-16 20:25:42 -0500

    use_raycast now a toggable option

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,16 +4,17 @@ extends CharacterBody2D
 @onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
 
 @export var speed = 75
+@export var use_raycast = true
+
 var target
 var map
 var space_state
-var nav_agent
+var nav_agent = false
 
 func _ready():
 	target = global_position
 	map = get_node("/root/Node2D/TileMapLayer") 
 	space_state = get_world_2d().direct_space_state
-	nav_agent = false
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
@@ -21,11 +22,15 @@ func _input(event):
 		var cell_coord = map.local_to_map(mouse_pos)
 		if (map.get_cell_tile_data(cell_coord)):
 			target = mouse_pos
-#			# Use raycast to check if collisions. If so, use NavigationAgent
-			var query = PhysicsRayQueryParameters2D.create(global_position, target)
-			var result = space_state.intersect_ray(query)
-			if result:
-				nav_agent = true
+			
+			if use_raycast:
+	#			# Use raycast to check if collisions. If so, use NavigationAgent
+				var query = PhysicsRayQueryParameters2D.create(global_position, target)
+				var result = space_state.intersect_ray(query)
+				if result:
+					nav_agent = true
+					navigation_agent_2d.target_position = target
+			else:
 				navigation_agent_2d.target_position = target
 			
 			
@@ -33,7 +38,7 @@ func _physics_process(delta):
 	velocity = global_position.direction_to(target) * speed
 	
 #	TODO: Gets stuck on walls and movement completely stops
-	if nav_agent:
+	if nav_agent or !use_raycast:
 		if navigation_agent_2d.is_navigation_finished():
 			nav_agent = false
 			return
